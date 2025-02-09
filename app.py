31b5d07d225994b0a86fa653fecf9723ecf7b15c
Saber Ghasemtabar <gh.saber@gmail.com>
2025-02-09 11:48:48 +0100

    multiple transactions

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ def home():
 # Define the prediction endpoint
 @app.post("/predict/")
 def predict(data: dict):
-    features = np.array([data["features"]])
-    prediction = model.predict(features)
-    return {"fraud_prediction": int(prediction[0])}
+    features = np.array(data["features"])
+    predictions = model.predict(features)
+    return {"fraud_predictions": predictions.tolist()}
 @app.get("/predict/")
 def predict_info():
     return {"message": "Please send a POST request with transaction features to get a prediction. Use /docs/ Swagger UI"}
